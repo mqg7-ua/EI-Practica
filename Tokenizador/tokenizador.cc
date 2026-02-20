80e74c4a7b93849768616aa5729269abcb333cc9
mqg7-ua <mqg7@alu.ua.es>
2026-02-20 14:23:35 +0100

    hecho casi todo lo facil

--- Tokenizador/tokenizador.cc
+++ Tokenizador/tokenizador.cc
@@ -14,11 +14,7 @@ using namespace std;
 */
 Tokenizador::Tokenizador (const string& delimitadoresPalabra, const& boolkcasosEspeciales, const bool& minuscSinAcentos){
     delimiters.clear();
-    for (char c : delimitadoresPalabra){
-        if(delimiters.find(c) = std::string::npos){
-            delimiters.push_back(c);
-        }
-    }
+    DelimitadoresPalabra(DelimitadoresPalabra);
     casosEspeciales = kcasosEspeciales; 
     pasarAminuscSinAcentos = minuscSinAcentos;
 }
@@ -128,17 +124,28 @@ bool Tokenizador::Tokenizar (const string& i, const string& f) const{
             añadiéndole extensión .tk (sin eliminar previamente la extensión de i
             por ejemplo, del archivo pp.txt se generaría el resultado en pp.txt.tk),
             y que contendrá una palabra en cada línea del fichero. Devolverá true si
-            se realiza la tokenización de forma correcta; false en caso contrario enviando a cerr el mensaje correspondiente (p.ej. que no exista el
-            archivo i)
-*@param i: 
-*@return true: 
-*@return false: 
+            se realiza la tokenización de forma correcta; false en caso contrario enviando 
+            a cerr el mensaje correspondiente (p.ej. que no exista el archivo i)
+*@param i: fichero entrada
 */
 bool Tokenizador::Tokenizar (const string & i) const{
-
+    return Tokenizar(i, i + ".tk");
 }
 
-//La eficiencia es importante en esta (y lo que cuelgue de aqui)
+//La eficiencia es importante en esta (y lo que cuelgue de aqui)////////////////////////////////77
+/**
+*@brief Tokeniza el fichero i que contiene un nombre de fichero por línea
+        guardando la salida en ficheros (uno por cada línea de i) cuyo nombre
+        será el leído en i añadiéndole extensión .tk, y que contendrá una
+        palabra en cada línea del fichero leído en i. Devolverá true si se
+        realiza la tokenización de forma correcta de todos los archivos que
+        contiene i; devolverá false en caso contrario enviando a cerr el mensaje
+        correspondiente (p.ej. que no exista el archivo i, o que se trate de un
+        directorio, enviando a ?cerr? los archivos de i que no existan o que
+        sean directorios; luego no se ha de interrumpir la ejecución si hay
+        algún archivo en i que no exista)
+*@param i: fichero de entrada 
+ */
 bool Tokenizador::TokenizarListaFicheros (const string& i) const{
 
 }
@@ -162,42 +169,66 @@ bool Tokenizador::TokenizarDirectorio (const string& i) const{
   /////////////////////////////////////////
  //             DELIMITADORES           //
 /////////////////////////////////////////
-
+/**
+*@brief Inicializa delimiters a nuevoDelimiters, filtrando que no se
+        introduzcan delimitadores repetidos (de izquierda a derecha, en cuyo
+        caso se eliminarían los que hayan sido repetidos por la derecha)
+*@param nuevoDelimiters: delimitadores nuevos para poner en this->delimiters. 
+ */
 void Tokenizador::DelimitadoresPalabra(const string& nuevoDelimiters){
-
+    delimiters.clear();
+    AnyadirDelimitadoresPalabra(nuevoDelimiters);
+    
 }
-
+/**
+*@brief: añade nuevodelimiters
+*@param nuevoDelimiters: delimitadores que añadir. 
+ */
 void Tokenizador::AnyadirDelimitadoresPalabra(const string& nuevoDelimiters){
-
+    for (char c : nuevoDelimiters){
+        if(delimiters.find(c) == string::npos){
+            delimiters.push_back(c);
+        }
+    }
 }
 
+/**
+* @return delimiters 
+*/
 string Tokenizador::DelimitadoresPalabra() const{
-
-}
+    return delimiters; 
+}   
 
 
   /////////////////////////////////////////
- //          CASOS ESPECIALES           //
+ //        Getters && Setters           //
 /////////////////////////////////////////
 
+//Setter casosEspeciales
 void Tokenizador::CasosEspeciales (const bool& nuevoCasosEspeciales){
-
+    casosEspeciales = nuevoCasosEspeciales; 
 }
 
+//getter casosespeciales
 bool Tokenizador::CasosEspeciales (){
-
+    return casosEspeciales;
 }
 
-
-  ///////////////////////////
- //     MIN Y MAYUS       //
-///////////////////////////
+/**
+*@brief:Cambia la variable privada ?pasarAminuscSinAcentos?. Atención al
+        formato de codificación del corpus (comando ?file? de Linux). Para la
+        corrección de la práctica se utilizará el formato actual (ISO-8859)
+* @param nuevoPasarAminuscSinAcentos 
+ */
 void Tokenizador::PasarAminuscSinAcentos (const bool& nuevoPasarAminuscSinAcentos){
-
+    pasarAminuscSinAcentos = nuevoPasarAminuscSinAcentos; 
 }
 
+/**
+*@brief:Devuelve el contenido de la variable privada ?pasarAminuscSinAcentos?
+ */
 bool Tokenizador::PasarAminuscSinAcentos (){
-
+    return pasarAminuscSinAcentos;
 }
 
 
@@ -205,7 +236,7 @@ bool Tokenizador::PasarAminuscSinAcentos (){
  //            OP SALIDA                //
 /////////////////////////////////////////
 ostream& operator<<(ostream&, const Tokenizador&){
-
+return *this; //////////////////para compilar 
 
 
 }
